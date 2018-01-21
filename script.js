8acd4e78f17b53acdabab6a25eeb17b104b2fbc0
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-01-21 16:31:59 +0100

    add styles

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
